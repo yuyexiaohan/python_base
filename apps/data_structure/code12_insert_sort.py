b4e6ab7ec955347cdf20798019067e369957b31e
achjiang <htxz_jiang@163.com>
2018-09-21 17:03:47 +0800

    Add the result

--- apps/data_structure/code12_insert_sort.py
+++ apps/data_structure/code12_insert_sort.py
@@ -8,9 +8,20 @@ def insert_sort(alist):
 	"""插入排序"""
 	n = len(alist)
 	# 从右边的无序序列中取出多个元素进行
-	for j in range(1, n):
+	# for j in range(1, n):
+	# 	# i代表内存循环起始值
+	# 	i = j
+	# 	# 执行从右边的无序序列中取出的第一个元素，即i位置的元素，然后将其插入到前面的正确位置
+	# 	while i > 0:
+	# 		if alist[i] < alist[i-1]:
+	# 			alist[i], alist[i-1] = alist[i-1], alist[i]
+	# 			i -= 1
+	# 		else:
+	# 			break
+
+	for i in range(1, n):
 		# i代表内存循环起始值
-		i = j
+		# i = j
 		# 执行从右边的无序序列中取出的第一个元素，即i位置的元素，然后将其插入到前面的正确位置
 		while i > 0:
 			if alist[i] < alist[i-1]:
