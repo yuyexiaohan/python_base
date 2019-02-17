178a96ba034ae986482b0af72d5b798224460bf7
achjiang <htxz_jiang@163.com>
2019-02-17 20:38:45 +0800

    add #04

--- apps/offer_algorithm/04-重建二叉树.py
+++ apps/offer_algorithm/04-重建二叉树.py
@@ -1,3 +1,32 @@
 # coding=utf-8 
 # Time : 2019/2/14
 # Author : achjiang
+"""
+输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, tin):
+        # write code here
+        if not pre and not tin:
+            return None
+        root = TreeNode(pre[0])
+        if set(pre) != set(tin):
+            return None
+        i = tin.index(pre[0])
+        root.left = self.reConstructBinaryTree(pre[1:i+1],tin[:i])
+        root.right = self.reConstructBinaryTree(pre[i+1:], tin[i+1:])
+        return root
+
+ss = Solution()
+li1 = [1,2,4,7,3,5,6,8]
+li2 = [4,7,2,1,5,3,8,6]
+li = ss.reConstructBinaryTree(li1, li2)
+print(li)
