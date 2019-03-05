3a23f20ac471902f6cc622fe64fc731e79a8676c
achjiang <htxz_jiang@163.com>
2019-03-05 23:16:48 +0800

    add #08

--- apps/offer_algorithm/08-跳台阶.py
+++ apps/offer_algorithm/08-跳台阶.py
@@ -7,7 +7,22 @@
 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）
 """
 
+
 class Solution:
-    def jumpFloor(self, number):
-        # write code here
-		pass
+	def JumpFloor(self, number):
+		# write code here
+		res = [1, 2]
+		if number <= 0 or not isinstance(number, int):
+			return 0
+		elif number == 1:
+			return 1
+		elif number == 2:
+			return 2
+		else :
+			for i in range(3, number+1):
+				res[(i + 1) % 2] = res[0] + res[1]
+			return res[(number + 1) % 2]
+
+ss = Solution()
+a = ss.JumpFloor(5)
+print(a)
